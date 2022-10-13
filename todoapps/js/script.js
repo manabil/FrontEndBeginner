f77ecada899fbb6caeb70421ad4e34a8cec4f1d6
manabil <mammarnabil@students.unnes.ac.id>
2022-10-13 08:42:48 +0700

    Add show and add feature in todolist app

--- todoapps/js/script.js
+++ todoapps/js/script.js
@@ -9,30 +9,132 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+/**
+ * Add todo item
+ */
 function addTodo() {
+  /**
+   * Generate unix timestamp
+   * @returns {number} Timestamp in unix format
+   */
+  function generateId() {
+    return +new Date();
+  }
+
+  /**
+   * Generate object of task
+   * @param {number} id Num of id
+   * @param {string} task Description of task
+   * @param {string} timestamp Deadline date of task
+   * @param {boolean} isCompleted Is task completed or not
+   * @returns {object} Object of generateTodoObject
+   */
+  function generateTodoObject(id, task, timestamp, isCompleted) {
+    return {
+      id,
+      task,
+      timestamp,
+      isCompleted,
+    };
+  }
   const textTodo = document.getElementById("title").value;
   const timestamp = document.getElementById("date").value;
 
   const generatedID = generateId();
-  const todoObject = generateTodoObject(generatedID, textTodo, timestamp, false);
+  const todoObject = generateTodoObject(
+    generatedID,
+    textTodo,
+    timestamp,
+    false
+  );
   todos.push(todoObject);
 
   document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
-function generateId() {
-  return +new Date();
-}
+/**
+ * Show todo item list
+ * @param {object} todoObject Object of generateTodoObject
+ * @returns {HTMLCollection} Element of todo list
+ */
+function makeTodo(todoObject) {
+  /**
+   * Return an object if avaiable and return null
+   * if object unavaiable
+   * @param {number} todoId Unix timestamps of todolist
+   * @returns {object} Object of todo item
+   */
+  function findTodo(todoId) {
+    for (const todoItem of todos) {
+      if (todoItem.id === todoId) {
+        return todoItem;
+      }
+    }
+    return null;
+  }
 
-function generateTodoObject(id, task, timestamp, isCompleted) {
-  return {
-    id,
-    task,
-    timestamp,
-    isCompleted,
-  };
+  /**
+   * Return a todo item to completed list
+   * @param {number} todoId TodoId from unix timestamp
+   * @returns {Event}
+   */
+  function addTaskToCompleted(todoId) {
+    const todoTarget = findTodo(todoId);
+    if (todoTarget == null) return;
+    todoTarget.isCompleted = true;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  const textTitle = document.createElement("h2");
+  textTitle.innerText = todoObject.task;
+
+  const textTimestamp = document.createElement("p");
+  textTimestamp.innerText = todoObject.timestamp;
+
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("inner");
+  textContainer.append(textTitle, textTimestamp);
+
+  const container = document.createElement("div");
+  container.classList.add("item", "shadow");
+  container.append(textContainer);
+  container.setAttribute("id", `todo-${todoObject.id}`);
+
+  if (todoObject.isCompleted) {
+    const undoButton = document.createElement("button");
+    undoButton.classList.add("undo-button");
+    undoButton.addEventListener("click", function () {
+      undoTaskFromCompleted(todoObject.id);
+    });
+
+    const trashButton = document.createElement("button");
+    trashButton.classList.add("trash-button");
+    trashButton.addEventListener("click", function () {
+      removeTaskFromCompleted(todoObject.id);
+    });
+
+    container.append(undoButton, trashButton);
+  } else {
+    const checkButton = document.createElement("button");
+    checkButton.classList.add("check-button");
+    checkButton.addEventListener("click", function () {
+      addTaskToCompleted(todoObject.id);
+    });
+
+    container.append(checkButton);
+  }
+
+  return container;
 }
 
 document.addEventListener(RENDER_EVENT, function () {
-  console.log(todos);
+  const uncompletedTODOList = document.getElementById("todos");
+  uncompletedTODOList.innerHTML = "";
+
+  for (const todoItem of todos) {
+    const todoElement = makeTodo(todoItem);
+    if (!todoItem.isCompleted) {
+      uncompletedTODOList.append(todoElement);
+    }
+  }
 });
