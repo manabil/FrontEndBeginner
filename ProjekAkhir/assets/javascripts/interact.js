dcbaf6614a9b34706dbe89a5cffbf3f3b94d2123
manabil <mammarnabil@students.unnes.ac.id>
2022-11-03 12:40:12 +0700

    Fix style code js

--- ProjekAkhir/assets/javascripts/interact.js
+++ ProjekAkhir/assets/javascripts/interact.js
@@ -1,40 +1,38 @@
 let clicked = true;
-let hamburger = document.querySelector(".hamburger");
-let button = document.querySelector(".menu");
-let nav = document.querySelector("nav");
+const hamburger = document.querySelector('.hamburger');
+const button = document.querySelector('.menu');
+const nav = document.querySelector('nav');
 let initialScroll = window.scroll;
 
-button.addEventListener("click", () => {
-  hamburger.classList.toggle("slided");
+button.addEventListener('click', () => {
+  hamburger.classList.toggle('slided');
+  if (clicked) {
+    clicked = false;
+    button.firstElementChild.style.transformOrigin = '0 0';
+    // eslint-disable-next-line max-len
+    button.firstElementChild.style.transform = 'rotate(45deg) translate(-1px, -6px)';
+    button.lastElementChild.style.transformOrigin = '0 0';
+    button.lastElementChild.style.transform = 'rotate(-45deg)';
+    button.children[1].style.transform = 'scale(0)';
+  } else {
+    clicked = true;
+    button.firstElementChild.style.transformOrigin = '0 0';
+    button.firstElementChild.style.transform = 'rotate(0)';
+    button.lastElementChild.style.transformOrigin = '0 0';
+    button.lastElementChild.style.transform = 'rotate(0)';
+    button.children[1].style.transform = 'scale(1)';
+  }
 });
 
 /**
  * Hide/show a navbar list in mobile
  */
-function toggler() {
-  if (clicked) {
-    clicked = false;
-    button.firstElementChild.style.transformOrigin = "0 0";
-    button.firstElementChild.style.transform = "rotate(45deg) translate(-1px, -6px)";
-    button.lastElementChild.style.transformOrigin = "0 0";
-    button.lastElementChild.style.transform = "rotate(-45deg)";
-    button.children[1].style.transform = "scale(0)";
-  } else {
-    clicked = true;
-    button.firstElementChild.style.transformOrigin = "0 0";
-    button.firstElementChild.style.transform = "rotate(0)";
-    button.lastElementChild.style.transformOrigin = "0 0";
-    button.lastElementChild.style.transform = "rotate(0)";
-    button.children[1].style.transform = "scale(1)";
-  }
-}
-
-window.onscroll = () => {
-  var currentScroll = window.pageYOffset;
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
   if (initialScroll > currentScroll) {
-    nav.style.top = "0";
+    nav.style.top = '0';
   } else {
-    nav.style.top = "-70px";
+    nav.style.top = '-70px';
   }
   initialScroll = currentScroll;
-};
+});
