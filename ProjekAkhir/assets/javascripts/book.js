39ce6e06b190d6b6f86c994661f07c028e0149fa
manabil <mammarnabil@students.unnes.ac.id>
2022-11-10 00:36:48 +0700

    Final bug fix

--- ProjekAkhir/assets/javascripts/book.js
+++ ProjekAkhir/assets/javascripts/book.js
@@ -5,16 +5,40 @@ const STORAGE_KEY = 'CATALOG_APPS';
 
 document.addEventListener('DOMContentLoaded', () => {
   const submitForm = document.getElementById('form');
-  if (submitForm != null) {
-    submitForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      addBook();
-    });
-  }
   if (isStorageExist()) {
     loadDataFromStorage();
   }
+  if (submitForm != null) {
+    const params = new URLSearchParams(document.location.search);
+    if (params.has('book')) {
+      getBook(parseInt(params.get('book')));
+      submitForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        editBook(parseInt(params.get('book')));
+      });
+    } else {
+      submitForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        addBook();
+      });
+    }
+  }
 });
+
+/**
+ * Return an index if avaiable in book object and return -1
+ * if index unavaiable in book object
+ * @param {number} bookId Unix timestamps of booklist
+ * @return {number} Index of bookId
+ */
+function findBookIndex(bookId) {
+  for (const index in books) {
+    if (books[index].id === bookId) {
+      return index;
+    }
+  }
+  return false;
+}
 
 /**
  * Insert a book if no data loaded
@@ -64,6 +88,37 @@ function addBook() {
 }
 
 /**
+ * Edit book
+ * @param {number} bookId Id of book
+ */
+function editBook(bookId) {
+  const inputTitle = document.getElementById('inputJudul').value;
+  const inputAuthor = document.getElementById('inputPenulis').value;
+  const inputYear = document.getElementById('inputTahun').value;
+  // eslint-disable-next-line max-len
+  const inputIsComplete = document.querySelector('input[name="inputStatusBuku"]:checked').value === 'true' ? true : false;
+
+  const bookIdx = findBookIndex(bookId);
+  books[bookIdx].title = inputTitle + ' ';
+  books[bookIdx].author = inputAuthor;
+  books[bookIdx].year = inputYear;
+  books[bookIdx].isCompleted = inputIsComplete;
+
+  Swal.fire({
+    title: 'Apakah anda yakin mengubah buku ini ?',
+    showCancelButton: true,
+    confirmButtonText: 'Ya',
+    cancelButtonText: 'Batal',
+    icon: 'warning',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      toastNotification('edit');
+      saveData();
+    }
+  });
+}
+
+/**
  * Show book item list
  * @param {object} bookObject Object of generateBookObject
  * @return {HTMLCollection} Element of book list
@@ -82,21 +137,6 @@ function manipulateBook(bookObject) {
       }
     }
     return null;
-  }
-
-  /**
-   * Return an index if avaiable in book object and return -1
-   * if index unavaiable in book object
-   * @param {number} bookId Unix timestamps of booklist
-   * @return {number} Index of bookId
-   */
-  function findBookIndex(bookId) {
-    for (const index in books) {
-      if (books[index].id === bookId) {
-        return index;
-      }
-    }
-    return false;
   }
 
   /**
@@ -135,79 +175,80 @@ function manipulateBook(bookObject) {
     saveData();
   }
 
-  // ! BELUUUM SELESAI FUNCTION INI
-  /**
-   * Edit book item
-   * @param {number} bookId TodoId from unix timestamp
-   */
-  function editBook(bookId) {
-    const bookTarget = findBook(bookId);
-    if (bookTarget == null) return;
-    // pass
-    document.dispatchEvent(new Event(RENDER_EVENT));
-    saveData();
-  }
-
   const bookTitle = document.createElement('h2');
-  bookTitle.innerText = bookObject.title;
-
   const bookYear = document.createElement('p');
+  const bookAuthor = document.createElement('p');
+
+  bookTitle.innerText = bookObject.title;
+  bookAuthor.innerText = bookObject.author;
   bookYear.innerText = bookObject.year;
   bookYear.style.fontWeight = 900;
   bookYear.style.fontSize = '17px';
   bookYear.style.marginBottom = '15px';
 
-  const bookAuthor = document.createElement('p');
-  bookAuthor.innerText = bookObject.author;
-
   const bookInfo = document.createElement('div');
-  bookInfo.classList.add('book-info');
-  bookInfo.append(bookTitle, bookYear, bookAuthor);
-
   const bookAction = document.createElement('div');
+  bookInfo.classList.add('book-info');
   bookAction.classList.add('book-action');
+  bookInfo.append(bookTitle, bookYear, bookAuthor);
 
   const container = document.createElement('div');
   container.classList.add('book-card');
   container.append(bookInfo);
 
   const iconReaded = document.createElement('i');
-  iconReaded.classList.add('fa', 'fa-light', 'fa-book-open', 'fa-2xl');
   const iconUnreaded = document.createElement('i');
-  iconUnreaded.classList.add('fa', 'fa-thin', 'fa-book', 'fa-2xl');
   const iconEdit = document.createElement('i');
-  iconEdit.classList.add('fa', 'fa-regular', 'fa-pen-to-square', 'fa-2xl');
   const iconDelete = document.createElement('i');
-  iconDelete.classList.add('fa', 'fa-regular', 'fa-trash', 'fa-2xl');
   const iconCheck = document.createElement('i');
+
+  iconReaded.classList.add('fa', 'fa-light', 'fa-book-open', 'fa-2xl');
+  iconUnreaded.classList.add('fa', 'fa-thin', 'fa-book', 'fa-2xl');
+  iconEdit.classList.add('fa', 'fa-regular', 'fa-pen-to-square', 'fa-2xl');
+  iconDelete.classList.add('fa', 'fa-regular', 'fa-trash', 'fa-2xl');
   iconCheck.classList.add('fa', 'fa-light', 'fa-circle-check');
 
   const buttonEdit = document.createElement('button');
-  buttonEdit.title = 'Edit Book';
-  buttonEdit.append(iconEdit);
   const buttonDelete = document.createElement('button');
+
+  buttonEdit.title = 'Edit Book';
   buttonDelete.title = 'Delete Book';
+
+  buttonEdit.append(iconEdit);
   buttonDelete.append(iconDelete);
 
   buttonEdit.addEventListener('click', () => {
-    editBook(bookObject.id);
+    window.open(`tambah.html?book=${bookObject.id}`, '_parent');
   });
+
   buttonDelete.addEventListener('click', () => {
-    removeBook(bookObject.id);
+    Swal.fire({
+      title: 'Apakah anda ingin menghapus buku ini ?',
+      showCancelButton: true,
+      confirmButtonText: 'Ya',
+      cancelButtonText: 'Batal',
+      icon: 'warning',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        toastNotification('hapus', bookObject);
+        removeBook(bookObject.id);
+      }
+    });
   });
 
   if (bookObject.isCompleted) {
     const readedCheck = document.createElement('span');
+    const buttonUncompleted = document.createElement('button');
     readedCheck.style.color = 'green';
+    buttonUncompleted.title = 'Uncomplete Read';
+
     readedCheck.append(iconCheck);
     bookTitle.append(readedCheck);
-
-    const buttonUncompleted = document.createElement('button');
-    buttonUncompleted.title = 'Uncomplete Read';
     buttonUncompleted.append(iconReaded);
 
     buttonUncompleted.addEventListener('click', () => {
       undoBook(bookObject.id);
+      toastNotification('ubah', bookObject);
     });
 
     bookAction.append(buttonUncompleted, buttonEdit, buttonDelete);
@@ -219,6 +260,7 @@ function manipulateBook(bookObject) {
 
     buttonCompleted.addEventListener('click', () => {
       readedBook(bookObject.id);
+      toastNotification('ubah', bookObject);
     });
 
     bookAction.append(buttonCompleted, buttonEdit, buttonDelete);
@@ -268,24 +310,166 @@ function loadDataFromStorage() {
 /**
  * Toast a notification
  * @param {string} text Information of notification
+ * @param {string} buku Object of book
  */
-function toastNotification(text = 'tambah') {
-  if (text != 'tambah') {
+function toastNotification(text, buku) {
+  if (text == 'hapus') {
     // eslint-disable-next-line max-len
-    tata.success(`Buku telah berhasil di${text}`, '', {
+    tata.success(`Buku telah berhasil dihapus`, `Buku "${buku.title}" telah dihapus`, {
       duration: 5000,
       animate: 'slide',
     });
+  } else if (text == 'ubah') {
+    // eslint-disable-next-line max-len
+    if (buku.isCompleted) {
+      tata.success(`Buku sudah dibaca`, `Buku "${buku.title}" telah dibaca`, {
+        duration: 5000,
+        animate: 'slide',
+      });
+    } else {
+      tata.warn(`Buku belum dibaca`, `Buku "${buku.title}" belum dibaca`, {
+        duration: 5000,
+        animate: 'slide',
+      });
+    }
+  } else if (text == 'edit') {
+    // eslint-disable-next-line max-len
+    tata.success(`Buku berhasil diubah`, 'Klik disini untuk melihat katalog', {
+      duration: 5000,
+      animate: 'slide',
+      onClick: () => {
+        window.open('katalog.html');
+      },
+    });
+  } else {
+    // eslint-disable-next-line max-len
+    tata.success(`Buku berhasil ditambah`, 'Klik disini untuk melihat katalog', {
+      duration: 5000,
+      animate: 'slide',
+      onClick: () => {
+        window.open('katalog.html');
+      },
+    });
   }
-  // eslint-disable-next-line max-len
-  tata.success(`Buku berhasil ditambah`, 'Klik disini untuk melihat katalog', {
-    duration: 5000,
-    animate: 'slide',
-    onClick: () => {
-      window.open('katalog.html');
-    },
-  });
 }
+
+/**
+ * jos
+ */
+function searchBook() {
+  // eslint-disable-next-line max-len
+  const searchInput = document.getElementById('searchInput').value.toLowerCase();
+  const container = document.getElementsByClassName('book-item')[0];
+  container.innerHTML = '';
+
+  if (searchInput == '') {
+    document.dispatchEvent(new Event(RENDER_EVENT));
+    return;
+  }
+  let isAny = 0;
+  for (const book of books) {
+    // eslint-disable-next-line max-len
+    if (book.title.toLowerCase().includes(searchInput) || book.year.toLowerCase().includes(searchInput) || book.author.toLowerCase().includes(searchInput)) {
+      if (book.isCompleted) {
+        const item = `
+          <div class="book-card">
+            <div class="book-info">
+              <h2>${book.title} 
+                <span style="color: green">
+                  <i class="fa fa-light fa-circle-check"></i>
+                </span>
+              </h2>
+              <p style="font-size: 17px"><b>${book.year}</b></p>
+              <p style="margin-top: 15px;">${book.author}</p>
+            </div>
+            <div class="book-action">
+              <button title="Uncomplete Read">
+                <i class="fa fa-light fa-book-open fa-2xl"></i>
+              </button>
+              <button title="Edit Book">
+                <i class="fa fa-regular fa-pen-to-square fa-2xl"></i>
+              </button>
+              <button title="Delete Book">
+                <i class="fa fa-regular fa-trash fa-2xl"></i>
+              </button>
+            </div>
+          </div>`;
+        container.innerHTML += item;
+        isAny += 1;
+      } else {
+        const item = `
+          <div class="book-card">
+            <div class="book-info">
+              <h2>${book.title}</h2>
+              <p style="font-size: 17px"><b>${book.year}</b></p>
+              <p style="margin-top: 15px;">${book.author}</p>
+            </div>
+            <div class="book-action">
+              <button title="Completed Read">
+                <i class="fa fa-thin fa-book fa-2xl"></i>
+              </button>
+              <button title="Edit Book">
+                <i class="fa fa-regular fa-pen-to-square fa-2xl"></i>
+              </button>
+              <button title="Delete Book">
+                <i class="fa fa-regular fa-trash fa-2xl"></i>
+              </button>
+            </div>
+          </div>`;
+        container.innerHTML += item;
+        isAny += 1;
+      }
+    }
+  }
+  if (!isAny) {
+    const text = document.createElement('h3');
+    text.innerHTML = 'Tidak ditemukan buku ! </br> ❌';
+    text.style.marginTop = '50px';
+    text.style.textAlign = 'center';
+    text.style.fontWeight = 'bold';
+    text.style.color = 'red';
+    container.append(text);
+    isAny = false;
+  }
+}
+
+/**
+ * Get book and insert value to edit book
+ * @param {number} bookId Id of book
+ */
+function getBook(bookId) {
+  const submitButton = document.getElementsByName('saveButton')[0];
+  const inputJudul = document.getElementById('inputJudul');
+  const inputPenulis = document.getElementById('inputPenulis');
+  const inputTahun = document.getElementById('inputTahun');
+
+  const bookIdx = findBookIndex(bookId);
+  const judul = books[bookIdx].title;
+  const penulis = books[bookIdx].author;
+  const tahun = books[bookIdx].year;
+  const isCompleted = books[bookIdx].isCompleted;
+
+  inputJudul.value = judul;
+  inputPenulis.value = penulis;
+  inputTahun.value = tahun;
+  if (isCompleted) {
+    document.getElementById('inputComplete').checked = true;
+  } else {
+    document.getElementById('inputUncomplete').checked = true;
+  }
+
+  submitButton.value = 'Edit Buku';
+}
+
+document.getElementById('searchInput').addEventListener('keyup', (event) => {
+  event.preventDefault();
+  searchBook();
+});
+
+document.getElementById('searchSubmit').addEventListener('submit', (event) => {
+  event.preventDefault();
+  searchBook();
+});
 
 document.addEventListener(RENDER_EVENT, () => {
   const bookList = document.getElementsByClassName('book-item')[0];
